56787ab36219965d7aacef6a8c3fe85e2404eb1f
Harold AO <>
2021-04-04 18:07:04 +0000

    Update

--- script.js
+++ script.js
@@ -2,28 +2,29 @@ let faceUpCards = []
 
 function loadFaceUpCards(){
   faceUpCards= [
-    load_image("assets/Card-1.png"),
-    load_image("assets/Card-2.png"),
-    load_image("assets/Card-3.png"),
-    load_image("assets/Card-4.png"),
-    load_image("assets/Card-5.png"),
-    load_image("assets/Card-6.png"),
-    load_image("assets/Card-7.png"),
-    load_image("assets/Card-8.png"),
-    load_image("assets/Card-9.png"),
-    load_image("assets/Card-10.png"),
-    load_image("assets/Card-11.png"),
-    load_image("assets/Card-12.png"),
-    load_image("assets/Card-13.png"),
-    load_image("assets/Card-14.png"),
-    load_image("assets/Card-15.png"),
-    load_image("assets/Card-16.png"),
-    load_image("assets/Card-17.png"),
+    loadImage("assets/Card-1.png"),
+    loadImage("assets/Card-2.png"),
+    loadImage("assets/Card-3.png"),
+    loadImage("assets/Card-4.png"),
+    loadImage("assets/Card-5.png"),
+    loadImage("assets/Card-6.png"),
+    loadImage("assets/Card-7.png"),
+    loadImage("assets/Card-8.png"),
+    loadImage("assets/Card-9.png"),
+    loadImage("assets/Card-10.png"),
+    loadImage("assets/Card-11.png"),
+    loadImage("assets/Card-12.png"),
+    loadImage("assets/Card-13.png"),
+    loadImage("assets/Card-14.png"),
+    loadImage("assets/Card-15.png"),
+    loadImage("assets/Card-16.png"),
+    loadImage("assets/Card-17.png")
   ]
 }
 
 function setup (){
   createCanvas(window.innerWidth, window.innerHeight);
+  loadFaceUpCards();
 }
 
 function draw(){
