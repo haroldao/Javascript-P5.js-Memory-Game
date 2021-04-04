8ef70d2ee8b03c6cfc506aaf7b107237f65b4648
Harold AO <>
2021-04-04 18:01:56 +0000

    Add Assets (IMGs) + Load_image

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let faceUpCards = []
+
+function loadFaceUpCards(){
+  faceUpCards= [
+    load_image("assets/Card-1.png"),
+    load_image("assets/Card-2.png"),
+    load_image("assets/Card-3.png"),
+    load_image("assets/Card-4.png"),
+    load_image("assets/Card-5.png"),
+    load_image("assets/Card-6.png"),
+    load_image("assets/Card-7.png"),
+    load_image("assets/Card-8.png"),
+    load_image("assets/Card-9.png"),
+    load_image("assets/Card-10.png"),
+    load_image("assets/Card-11.png"),
+    load_image("assets/Card-12.png"),
+    load_image("assets/Card-13.png"),
+    load_image("assets/Card-14.png"),
+    load_image("assets/Card-15.png"),
+    load_image("assets/Card-16.png"),
+    load_image("assets/Card-17.png"),
+  ]
+}
+
 function setup (){
   createCanvas(window.innerWidth, window.innerHeight);
 }
