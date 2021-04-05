be22dd9e25377610cd47f334f5a874fc58a961b2
Harold AO <>
2021-04-05 00:54:53 +0000

    WIP

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let faceUpCards = []
+    faceDownCard = []
     deck = []
     cards = []
-    faceDownCard = []
+    flippedCards = []
+    cooldown = null
 
-const rows = 3
-      columns = 6
+const rows = 12
+      columns = 12
+
 
 class Card {
   constructor(x, y, faceUpImage){
@@ -14,7 +17,7 @@ class Card {
     this.height = 200
     this.faceUpImage = faceUpImage
     this.faceDownImage = faceDownCard
-    this.isFaceUp = true
+    this.isFaceUp = false
   }
 
   render(){
@@ -23,13 +26,15 @@ class Card {
     // strokeWeight(4)
     rect(this.x, this.y, this.width, this.height, 20)
 
-    if(this.isFaceUp){
-      image(this.faceUpImage, this.x, this.y, this.width, this.height)
-    }
-    else{
-      image(this.faceDownImage, this.x + 40, this.y + 40, 40, 40)
-    }
-    
+    this.isFaceUp ? image(this.faceUpImage, this.x, this.y, this.width, this.height) : image(this.faceDownImage, this.x + 40, this.y + 40, 40, 40)
+
+  }
+  setIsFaceUp (isFaceUp){
+    this.isFaceUp = isFaceUp
+  }
+  isUnderMouse(x, y){
+    return x >= this.x && x < this.x + this.width &&
+          y >= this.y && y < this.y + this.height
   }
 }
 
@@ -73,12 +78,26 @@ function createCards(){
     for(let j=0; j < rows; j++){
       //image
       let card = new Card(
-        i * 280 + 40,
-        j * 280 + 40,
+        i * 280 + 100,
+        j * 280 + 100,
         deck.pop()
       )
       cards.push(card)
     }
+  }
+}
+function drawResultOnWin() {
+  let matches = true
+ 
+  for(let i = 0; i < cards.length; i++)
+  {
+    matches = matches && cards[i].isMatch
+  }
+ 
+  if(matches) {
+    //code
+    fill(0, 0, 0)
+    text("Bravo tu as tout trouvé", 20, 360)
   }
 }
 
@@ -94,11 +113,51 @@ function setup (){
   createDeck(faceUpCards);
   createCards()
 }
-
+function updateLogic() {
+  if(cooldown && frameCount - cooldown > 30){
+    for(let i = 0; i < cards.length; i++) 
+    {
+      if(!cards[i].isMatch && cards[i].isFaceUp){
+         cards[i].setIsFaceUp(false)
+      }
+    }
+ 
+    // remettre la liste de cartes flipped
+    flippedCards = []
+    cooldown = null
+  }
+}
 function draw(){
   // background(220);
+  updateLogic()
   for(let i = 0; i < cards.length; i++){
     cards[i].render()
   }
+  drawResultOnWin()
 
+}
+
+function mouseClicked(){
+  for(let i=0; i < cards.length; i++){
+    if(cards[i].isUnderMouse(mouseX, mouseY)){
+      // click sur la carte
+      if(flippedCards.length < 2 && !cards[i].isFaceUp) {
+        // retournement de la carte
+        cards[i].setIsFaceUp(true) 
+        flippedCards.push(cards[i])
+
+        // verifier si les cartes sont similaire
+        if(flippedCards.length === 2){
+          if(flippedCards[0].faceUpImage === flippedCards[1].faceUpImage){
+            flippedCards[0].isMatch = true
+            flippedCards[1].isMatch = true
+          }
+
+          cooldown = frameCount
+        }
+
+
+      }
+    }
+  }
 }
